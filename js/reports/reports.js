6ff6d592aad0f84febcf3bed90d477236eab9c8e
oberdan <oberdan@craos.net>
2019-09-10 18:51:20 -0300

    Adequação do relatório com a visão do banco de dados

--- js/reports/reports.js
+++ js/reports/reports.js
@@ -5,7 +5,7 @@ let reports = [
                     titulo: "Relatório analítico de capturas LPR",
                     subtitulo: "Demonstrativo de frequências por ponto de acesso",
                     descricao: "Relatório das passagens por ponto de acesso. Apresenta o ponto de acesso, as datas e as frequências de captura para o período selecionado ou os últimos 30 dias.",
-                    origem: "/smart/public/diario_captura_analitico",
+                    origem: "/smart/public/report_diario_captura_analitico",
                     campos:[
                         {nome:"data", label:"Data", detail: true, type:"date", format:"DD/MM/YYYY"},
                         {nome:"cliente", label:"Cliente", group: true, type: "combo"},
@@ -15,9 +15,9 @@ let reports = [
                         {nome:"ident_hora", label:"Ident./Hora", totais: false, avgs: true, type:"number", format: '0,'},
                         {nome:"capturas", label:"Capturas", totais: true, avgs: true, type:"number", format: '0,'},
                         {nome:"capt_hora", label:"Capt./Hora", totais:false, avgs: true, type:"number", truncate: true},
-                        {nome:"avgprocessing_time_ms", label:"Méd. T. Process.", totais:false, avgs: true, type:"number", truncate: true},
-                        {nome:"minprocessing_time_ms", label:"Mín. T. Process.", totais:false, avgs: true, type:"number", truncate: true},
-                        {nome:"maxprocessing_time_ms", label:"Máx. T. Process.", totais:false, avgs: true, type:"number", truncate: true},
+                        {nome:"avgprocess", label:"Méd. T. Process.", totais:false, avgs: true, type:"number", truncate: true},
+                        {nome:"minprocess", label:"Mín. T. Process.", totais:false, avgs: true, type:"number", truncate: true},
+                        {nome:"maxprocess", label:"Máx. T. Process.", totais:false, avgs: true, type:"number", truncate: true},
                         {nome:"avgconfidence", label:"Méd. LPR", totais:false, avgs: true, type:"number", truncate: true},
                         {nome:"minconfidence", label:"Mín. LPR", totais:false, avgs: true, type:"number", truncate: true},
                         {nome:"maxconfidence", label:"Máx. LPR", totais:false, avgs: true, type:"number", truncate: true}
