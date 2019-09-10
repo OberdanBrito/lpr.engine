14c45c742471d462aa0a35d9cec5de4e9b6599a6
oberdan <oberdan@craos.net>
2019-09-10 18:32:28 -0300

    Melhora na lista de menus de contexto disponibilizados de acordo com o tipo de nó que o usuário está navegando; Adicionado as opções de controle dos terminais

--- js/operacoes/operacoes.js
+++ js/operacoes/operacoes.js
@@ -49,62 +49,49 @@ class Operacoes {
             onload: that.CarregaRedes(that)
         });
 
-        /*this.Tree.attachEvent("onDblClick", function (id) {
-
-            switch (that.Tree.getUserData(id, 'tipo')) {
-                case 'rede':
-                    that.CarregaUnidades(id);
-                    break;
-                case 'unidade':
-                    that.CarregaTerminais(id);
-                    break;
-            }
-
-            return true;
-        });*/
-
         this.Tree.attachEvent("onContextMenu", function (id, x, y) {
 
             let node = that.Tree.getUserData(id);
+            let menu_list = null;
 
-            let tipos_menu = {
-                lpr: [
-                    {id: "atualizar_rede", text: 'Atualizar', img: "atualizar.svg"},
-                    {id: "nova_rede", text: 'Adicionar nova rede...', img: "rede.svg"},
-                ],
-                rede: [
-                    {id: "editar_rede", text: 'Editar...', img: "rede.svg"},
-                    {id: "desativar_rede", text: 'Desativar registro', img: "remover.svg"},
-                    {type: "separator"},
-                    {id: "adicionar_unidade", text: 'Adicionar nova unidade...', img: "unidade.svg"},
-                ],
-                unidade: [
-                    {id: "editar_unidade", text: 'Editar...', img: "informacoes.svg"},
-                    {id: "desativar_unidade", text: 'Desativar registro', img: "remover.svg"},
-                    {type: "separator"},
-                    {id: "novo_terminal", text: 'Adicionar novo terminal...', img: "unidade.svg"},
-                ]
-            };
-
-            let menulist = null;
-
-            switch (node.tipo) {
-                case 'lpr':
-                    menulist = tipos_menu.lpr;
-                    break;
-
-                case 'rede':
-                    menulist = tipos_menu.rede;
-                    break;
-                case 'unidade':
-                    menulist = tipos_menu.unidade;
-                    break;
-            }
+            [
+                {
+                    tipo: 'lpr', lista: [
+                        {id: "atualizar_rede", text: 'Atualizar', img: "atualizar.svg"},
+                        {id: "nova_rede", text: 'Adicionar nova rede...', img: "rede.svg"}
+                    ]
+                },
+                {
+                    tipo: 'rede', lista: [
+                        {id: "editar_rede", text: 'Editar...', img: "rede.svg"},
+                        {id: "desativar_rede", text: 'Desativar registro', img: "remover.svg"},
+                        {type: "separator"},
+                        {id: "adicionar_unidade", text: 'Adicionar nova unidade...', img: "unidade.svg"}
+                    ]
+                },
+                {
+                    tipo: 'unidade', lista: [
+                        {id: "editar_unidade", text: 'Editar...', img: "informacoes.svg"},
+                        {id: "desativar_unidade", text: 'Desativar registro', img: "remover.svg"},
+                        {type: "separator"},
+                        {id: "adicionar_terminal", text: 'Adicionar novo terminal...', img: "unidade.svg"},
+                    ]
+                },
+                {
+                    tipo: 'terminal', lista: [
+                        {id: "editar_terminal", text: 'Editar...', img: "informacoes.svg"},
+                        {id: "desativar_terminal", text: 'Desativar', img: "remover.svg"}
+                    ]
+                }
+            ].filter(function (item) {
+                if (node.tipo === item.tipo)
+                    menu_list = item.lista
+            });
 
             let MenuContexto = new dhtmlXMenuObject({
                 icons_path: "./img/operacoes/menu/",
                 context: true,
-                items: menulist
+                items: menu_list
             });
 
             MenuContexto.attachEvent("onClick", function (id) {
@@ -116,21 +103,28 @@ class Operacoes {
                         new Rede().Adicionar();
                         break;
                     case 'editar_rede':
-                        new Rede(node.id).Editar();
+                        new Rede(node).Editar();
                         break;
                     case 'desativar_rede':
-                        new Rede(node.id).Desativar();
+                        new Rede(node).Desativar();
                         break;
                     case 'adicionar_unidade':
-                        new Unidade(node.id).Adicionar();
+                        new Unidade(node).Adicionar();
                         break;
                     case 'editar_unidade':
-                        new Unidade(node.id).Editar();
+                        new Unidade(node).Editar();
                         break;
                     case 'desativar_unidade':
-                        new Unidade(node.id).Desativar();
+                        new Unidade(node).Desativar();
                         break;
                     case 'adicionar_terminal':
+                        new Terminal(node).Adicionar();
+                        break;
+                    case 'editar_terminal':
+                        new Terminal(node).Editar();
+                        break;
+                    case 'desativar_terminal':
+                        new Terminal(node).Desativar();
                         break;
                 }
             });
@@ -188,19 +182,19 @@ class Operacoes {
                     let redeid = 're_' + rede.id;
 
                     response.dados.findIndex(function (item, index) {
-                       if (item.rede === rede.id) {
-                           let newid = 'un_' + item.id;
-                           tree.addItem(newid, item.nome, redeid, index);
-                           tree.setUserData(newid, 'id', item.id);
-                           tree.setUserData(newid, 'tipo', 'unidade');
-                           tree.setUserData(newid, 'nome', item.nome);
-                           tree.setIconColor(newid, '#405057');
-                           tree.setItemIcons(newid, {
-                               file: "fas fa-cube",
-                               folder_opened: "fas fa-cube",
-                               folder_closed: "fas fa-cube"
-                           })
-                       }
+                        if (item.rede === rede.id) {
+                            let newid = 'un_' + item.id;
+                            tree.addItem(newid, item.nome, redeid, index);
+                            tree.setUserData(newid, 'id', item.id);
+                            tree.setUserData(newid, 'tipo', 'unidade');
+                            tree.setUserData(newid, 'nome', item.nome);
+                            tree.setIconColor(newid, '#405057');
+                            tree.setItemIcons(newid, {
+                                file: "fas fa-cube",
+                                folder_opened: "fas fa-cube",
+                                folder_closed: "fas fa-cube"
+                            })
+                        }
                     });
                 });
 
@@ -213,20 +207,20 @@ class Operacoes {
 
         let tree = this.Tree;
 
-        this.info.api = "/smart/public/cliente_terminal";
+        this.info.api = "/smart/public/cliente_lista_terminais";
         this.info.Listar({
             callback: function (response) {
 
                 unidades.filter(function (unidade) {
 
-                    let unidadadeid = 'un_' + unidade.id;
+                    let unidadade_id = 'un_' + unidade.id;
 
                     response.dados.findIndex(function (item, index) {
 
                         if (item.unidade === unidade.id) {
 
                             let newid = 'tr_' + item.id;
-                            tree.addItem(newid, item.nome, unidadadeid, index);
+                            tree.addItem(newid, item.nome, unidadade_id, index);
                             tree.setUserData(newid, 'id', item.id);
                             tree.setUserData(newid, 'tipo', 'terminal');
                             tree.setUserData(newid, 'nome', item.nome);
