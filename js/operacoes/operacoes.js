dc0909bdb129ddfbae31a6dbfa67d79883ec6a37
oberdan <oberdan@craos.net>
2019-09-11 15:43:58 -0300

    Melhoria de código: identifica e executa a ação configurada ao menu de contexto

--- js/operacoes/operacoes.js
+++ js/operacoes/operacoes.js
@@ -52,81 +52,92 @@ class Operacoes {
         this.Tree.attachEvent("onContextMenu", function (id, x, y) {
 
             let node = that.Tree.getUserData(id);
-            let menu_list = null;
+            let menu_tipo = null;
+            let menu_lista = null;
 
-            [
+            let objetos = [
                 {
                     tipo: 'lpr', lista: [
-                        {id: "atualizar_rede", text: 'Atualizar', img: "atualizar.svg"},
-                        {id: "nova_rede", text: 'Adicionar nova rede...', img: "rede.svg"}
+                        {id: "atualizar_rede", text: 'Atualizar', img: "atualizar.svg", acao: function () {
+                                that.CarregaRedes(that);
+                            }},
+                        {id: "nova_rede", text: 'Adicionar nova rede...', img: "rede.svg", acao: function () {
+                            new Rede().Adicionar();
+                        }}
                     ]
                 },
                 {
                     tipo: 'rede', lista: [
-                        {id: "editar_rede", text: 'Editar...', img: "rede.svg"},
-                        {id: "desativar_rede", text: 'Desativar registro', img: "remover.svg"},
+                        {id: "editar_rede", text: 'Editar...', img: "rede.svg", acao: function () {
+                            new Rede(node).Editar();
+                        }},
+                        {id: "desativar_rede", text: 'Desativar registro', img: "remover.svg", acao: function () {
+                            new Rede(node).Desativar();
+                        }},
                         {type: "separator"},
-                        {id: "adicionar_unidade", text: 'Adicionar nova unidade...', img: "unidade.svg"}
+                        {id: "adicionar_unidade", text: 'Adicionar nova unidade...', img: "unidade.svg", acao: function () {
+                            new Unidade(node).Adicionar();
+                        }}
                     ]
                 },
                 {
                     tipo: 'unidade', lista: [
-                        {id: "editar_unidade", text: 'Editar...', img: "informacoes.svg"},
-                        {id: "desativar_unidade", text: 'Desativar registro', img: "remover.svg"},
-                        {type: "separator"},
-                        {id: "adicionar_terminal", text: 'Adicionar novo terminal...', img: "unidade.svg"},
+                        {id: "editar_unidade", text: 'Editar...', img: "informacoes.svg", acao: function () {
+                            new Unidade(node).Editar();
+                        }},
+                        {id: "desativar_unidade", text: 'Desativar registro', img: "remover.svg", acao: function () {
+                            new Unidade(node).Desativar();
+                        }},
+                    ]
+                },
+                {
+                    tipo: 'terminais', lista: [
+                        {id: "adicionar_terminal", text: 'Adicionar novo terminal...', img: "unidade.svg", acao: function () {
+                            new Terminal(node).Adicionar();
+                        }},
+                    ]
+                },
+                {
+                    tipo: 'agentes_lpr', lista: [
+                        {id: "adicionar_agente", text: 'Adicionar novo agente LPR...', img: "unidade.svg"},
                     ]
                 },
                 {
                     tipo: 'terminal', lista: [
-                        {id: "editar_terminal", text: 'Editar...', img: "informacoes.svg"},
-                        {id: "desativar_terminal", text: 'Desativar', img: "remover.svg"}
+                        {id: "editar_terminal", text: 'Editar...', img: "informacoes.svg", acao: function () {
+                            new Terminal(node).Editar();
+                        }},
+                        {id: "desativar_terminal", text: 'Desativar', img: "remover.svg", acao: function () {
+                            new Terminal(node).Desativar();
+                        }}
+                    ]
+                },
+                {
+                    tipo: 'agente', lista: [
+                        {id: "editar_agente", text: 'Editar...', img: "informacoes.svg"},
+                        {id: "desativar_agente", text: 'Desativar', img: "remover.svg"}
                     ]
                 }
-            ].filter(function (item) {
-                if (node.tipo === item.tipo)
-                    menu_list = item.lista
+            ];
+
+            objetos.filter(function (item) {
+                if (node.tipo === item.tipo) {
+                    menu_lista = item.lista;
+                    menu_tipo = item.tipo;
+                }
             });
 
             let MenuContexto = new dhtmlXMenuObject({
                 icons_path: "./img/operacoes/menu/",
                 context: true,
-                items: menu_list
+                items: menu_lista
             });
 
             MenuContexto.attachEvent("onClick", function (id) {
-                switch (id) {
-                    case 'atualizar_rede':
-                        that.CarregaRedes();
-                        break;
-                    case 'nova_rede':
-                        new Rede().Adicionar();
-                        break;
-                    case 'editar_rede':
-                        new Rede(node).Editar();
-                        break;
-                    case 'desativar_rede':
-                        new Rede(node).Desativar();
-                        break;
-                    case 'adicionar_unidade':
-                        new Unidade(node).Adicionar();
-                        break;
-                    case 'editar_unidade':
-                        new Unidade(node).Editar();
-                        break;
-                    case 'desativar_unidade':
-                        new Unidade(node).Desativar();
-                        break;
-                    case 'adicionar_terminal':
-                        new Terminal(node).Adicionar();
-                        break;
-                    case 'editar_terminal':
-                        new Terminal(node).Editar();
-                        break;
-                    case 'desativar_terminal':
-                        new Terminal(node).Desativar();
-                        break;
-                }
+                menu_lista.filter(function (item) {
+                    if (item.id === id && item.acao !==  undefined)
+                        item.acao.call();
+                });
             });
 
             MenuContexto.showContextMenu(x, y);
@@ -193,12 +204,33 @@ class Operacoes {
                                 file: "fas fa-cube",
                                 folder_opened: "fas fa-cube",
                                 folder_closed: "fas fa-cube"
-                            })
+                            });
+
+                            let terminais_id = newid+'_'+'terminais';
+                            tree.addItem(terminais_id, 'Terminais', newid, 0);
+                            tree.setUserData(terminais_id, 'tipo', 'terminais');
+                            tree.setIconColor(terminais_id, '#009d6b');
+                            tree.setItemIcons(terminais_id, {
+                                file: "fas fa-chalkboard-teacher",
+                                folder_opened: "fas fa-chalkboard-teacher",
+                                folder_closed: "fas fa-chalkboard-teacher"
+                            });
+
+                            let agentes_id = newid+'_'+'agente_lpr';
+                            tree.addItem(agentes_id, 'Agentes LPR', newid, 0);
+                            tree.setUserData(agentes_id, 'tipo', 'agentes_lpr');
+                            tree.setIconColor(agentes_id, '#352260');
+                            tree.setItemIcons(agentes_id, {
+                                file: "fas fa-video",
+                                folder_opened: "fas fa-video",
+                                folder_closed: "fas fa-video"
+                            });
                         }
                     });
                 });
 
                 that.CarregaTerminais(response.dados);
+                that.CarregaAgentesLPR(response.dados);
             }
         })
     }
@@ -213,7 +245,7 @@ class Operacoes {
 
                 unidades.filter(function (unidade) {
 
-                    let unidadade_id = 'un_' + unidade.id;
+                    let unidadade_id = 'un_' + unidade.id+'_'+'terminais';
 
                     response.dados.findIndex(function (item, index) {
 
@@ -226,9 +258,43 @@ class Operacoes {
                             tree.setUserData(newid, 'nome', item.nome);
                             tree.setIconColor(newid, '#249d28');
                             tree.setItemIcons(newid, {
-                                file: "fas fa-cubes",
-                                folder_opened: "fas fa-car",
-                                folder_closed: "fas fa-car"
+                                file: "fas fa-chalkboard-teacher",
+                                folder_opened: "fas fa-chalkboard-teacher",
+                                folder_closed: "fas fa-chalkboard-teacher"
+                            })
+                        }
+                    });
+                });
+            }
+        })
+    }
+
+    CarregaAgentesLPR(unidades) {
+
+        let tree = this.Tree;
+
+        this.info.api = "/smart/public/cliente_agente";
+        this.info.Listar({
+            callback: function (response) {
+
+                unidades.filter(function (unidade) {
+
+                    let unidadade_id = 'un_' + unidade.id+'_'+'agente_lpr';
+
+                    response.dados.findIndex(function (item, index) {
+
+                        if (item.unidade === unidade.id) {
+
+                            let newid = 'ag_' + item.id;
+                            tree.addItem(newid, item.nome, unidadade_id, index);
+                            tree.setUserData(newid, 'id', item.id);
+                            tree.setUserData(newid, 'tipo', 'agente');
+                            tree.setUserData(newid, 'nome', item.nome);
+                            tree.setIconColor(newid, '#533596');
+                            tree.setItemIcons(newid, {
+                                file: "fas fa-video",
+                                folder_opened: "fas fa-video",
+                                folder_closed: "fas fa-video"
                             })
                         }
                     });
